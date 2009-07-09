141b4308e7a1c0dba60ea6c27cfa2c971c0fa074
SND\mikeq_cp <SND\mikeq_cp@856ff616-a6d3-46b1-938b-83cc1fee1ebb>
2009-07-09 05:54:58 +0000

    new ami
    git-svn-id: https://ec2bootstrapper.svn.codeplex.com/svn@23791 856ff616-a6d3-46b1-938b-83cc1fee1ebb

--- Ec2Bootstrapperlib/CEc2Instance.cs
+++ Ec2Bootstrapperlib/CEc2Instance.cs
@@ -17,7 +17,7 @@ namespace Ec2Bootstrapperlib
 {
     public class CEc2Instance
     {
-        const string jwAmiImageId = "ami-6dbd5b04"; //"ami-0529ce6c";// 
+        const string jwAmiImageId = "ami-0de80964"; //"ami-0529ce6c";// 
         const string jwCertFile                   = "server.crt";
         const string jwSecurityGroupName          = "JWSecureEc2FileLoad";
         const string jwSecurityGroupDescription   = "Used for msi upload";
